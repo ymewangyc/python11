ec01d8d8cd16be8a3ed31f727d6b55e42118f406
wyc <wyc@example.com>
2017-12-19 11:13:00 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# awesome-python3-webapp
+第一个git项目
---
+++ test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from MysqlHelper import MysqlHelper
+
+mh = MysqlHelper('10.100.1.142', 'test', '123456', 'testDB', 'utf8')
+sql = "select * from user where name=%s"
+print(mh.find(sql, '小明'))
+print(mh.find(sql, '张三'))
+
+
+# mh = MysqlHelper('localhost', 'root', '123456', 'test', 'utf8')
+# mh = MysqlHelper('10.100.1.142', 'test', '123456', 'testDB', 'utf8')
+sql = "insert into user(name,password) values(%s,%s)"
+mh.cud(sql, ('李四', '123456'))
+mh.cud(sql, ('赵六', '123456'))
+mh.cud(sql, ('哈哈哈', '123456'))
+mh.cud(sql, ('顺丰', '123456'))
+mh.cud(sql, ('132sd', '123456'))
